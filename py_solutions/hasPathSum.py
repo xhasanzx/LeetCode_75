c60743c0a596ff25f347c9d8f8f01eaf3e4ffd35
xhasanzx <hasandarweshzx@gmail.com>
2026-04-12 19:44:25 +0200

    hasPathSum

---
+++ py_solutions/hasPathSum.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root: return False
+        
+        if not root.left and not root.right:
+            return root.val == targetSum
+
+        return self.hasPathSum(root.left, targetSum-root.val) or self.hasPathSum(root.right, targetSum-root.val)
+        
+
+if __name__ == "__main__":
+    root = TreeNode(5)
+    root.left = TreeNode(4)
+    root.right = TreeNode(8)
+    root.left.left = TreeNode(11)
+    root.left.left.left = TreeNode(7)
+    root.left.left.right = TreeNode(2)
+    root.right.left = TreeNode(13)
+    root.right.right = TreeNode(4)
+    root.right.right.right = TreeNode(1)
+
+    targetSum = 22
+
+    print(Solution().hasPathSum(root, targetSum))
